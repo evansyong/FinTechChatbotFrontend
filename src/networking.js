31001b4bf28ecd099c4e676ff7f4486caac520d2
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-10-01 22:52:38 +0900

    added axios network interface

---
+++ src/networking.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+})
+
+instance.interceptors.request.use((config) => {
+    config.headers["content-type"] = "application/json";
+    config.headers["nypcapikey"] = import.meta.env.VITE_BACKEND_API_KEY
+
+    return config;
+}, (err) => {
+    return Promise.reject(err);
+})
+
+export default instance;
