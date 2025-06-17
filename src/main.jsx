20cb360c5143b236533aa47fbd90f9562a4761ea
Evans Yong Cheng Yu <evansyong321c@gmail.com>
2025-06-17 14:56:37 +0800

    left only chatbot frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import Home from './Home.jsx'
-import VerifyOTP from './VerifyOTP.jsx'
-import Chat from './Chat.jsx'
-import Credits from './Credits.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import Home from "./Home.jsx";
+import VerifyOTP from "./VerifyOTP.jsx";
+import Chat from "./Chat.jsx";
+import Credits from "./Credits.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom-right' } }}>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/verifyOTP' element={<VerifyOTP />} />
-                <Route path='/chat' element={<Chat />} />
-                <Route path='/credits' element={<Credits />} />
-            </Routes>
-        </BrowserRouter>
-    </ChakraProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider
+    toastOptions={{ defaultOptions: { position: "bottom-right" } }}
+  >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>
+);
